8038781cc6ebe52954650cc010f7f316bf6a87bd
Keyurrr14 <keyurrathod9920@gmail.com>
2024-12-17 08:13:43 +0530

    Information Retrieval: Levenstein with Matrix added

--- Information Retrieval/Practs 5/LivensteinDistance.py
+++ Information Retrieval/Practs 5/LivensteinDistance.py
@@ -1,3 +1,29 @@
+# With Matrix
+def levenshtein(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    grid = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        grid[i][0] = i
+    for j in range(n + 1):
+        grid[0][j] = j
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                grid[i][j] = grid[i - 1][j - 1]
+            else:
+                grid[i][j] = 1 + min(grid[i - 1][j], grid[i][j - 1], grid[i - 1][j - 1])
+
+    for row in grid:
+        print(row)
+    return grid[m][n]
+
+print(f"Distance between cat and cut: {levenshtein('cat', 'cut')}")
+
+
+# With Recursion
 def levenshtein_distance(str1, str2):
     if len(str1) == 0:
         return len(str2)
@@ -12,7 +38,7 @@ def levenshtein_distance(str1, str2):
         levenshtein_distance(str1[1:], str2),
         levenshtein_distance(str1, str2[1:])
     )
-    
+
 word1 = "cat"
 word2 = "cut"
 print(f"Distance between {word1} and {word2}: {levenshtein_distance(word1, word2)}")
@@ -20,3 +46,5 @@ print(f"Distance between {word1} and {word2}: {levenshtein_distance(word1, word2
 word1 = "book"
 word2 = "back"
 print(f"Distance between {word1} and {word2}: {levenshtein_distance(word1, word2)}")
+
+
